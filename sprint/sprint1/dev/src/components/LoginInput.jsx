a71b46bfd08930f8ac412b9a6e1e49a934385ffd
akifpatel <aakifpatel08@gmail.com>
2021-10-18 14:33:51 +0530

    second

---
+++ sprint/sprint1/dev/src/components/LoginInput.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function LoginInput() {
+    return (
+        <div>
+            <input type="text" placeholder="Enter your Email" /><br />
+            <input type="password" placeholder="Enter your password" /><br />
+            <button>Submit</button>
+        </div>
+    )
+}
+
+export {LoginInput}
---
+++ sprint/sprint1/dev/src/components/SignupInput.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignupInput() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export {SignupInput}
