bdd489f54ac7159a0a50c3590eeb01c0bed1b9da
akifpatel <aakifpatel08@gmail.com>
2021-11-08 15:33:04 +0530

    controller

---
+++ sprint/sprint2/backend/src/controller/user.controller.js
@@ -0,0 +1,43 @@
+const User=require("../model/user.model")
+const jwt =require('josnwebtoken')
+require("dotenv").config()
+
+const newToken=(user)=>{
+    return jwt.sign({user:user},
+    process.env.JWT_SECRET_KEY)
+}
+const register=async(req,res)=>{
+    try{
+        let user=await User.findOne({email:req.body.email}).lean().exec()
+        if(user){
+            return res.status(400).json({status:"error",message:"user already exists"})
+        }
+        user=await User.create(req.body)
+        const token =newToken(user)
+        return res.status(201).json({user:user,token:token})
+    }
+    catch(err){
+        return res.status(500).json({err})
+    }
+}
+
+const login=async(req,res)=>{
+    try{
+        let user = await User.findOne({email:res.body.email}).lean().exec()
+        if(!user){
+            return res.status(400).json({status:"error",message:"user does not exists"})
+        }
+        const match=user.checkPassword(req.body.password)
+        if(!match){
+            return res.status(400).json({status:"error",message:"password not matched"})
+        }
+        const token=newToken(user)
+        return res.status(200).json({user:user,token:token})
+    }
+    catch(err){
+         return res.staus(500).json(err)
+    }
+    
+}
+
+module.exports={register,login}
--- sprint/sprint2/backend/src/server.js
+++ sprint/sprint2/backend/src/server.js
@@ -1,8 +1,11 @@
 const express=require("express")
 const connect = require("./config/db")
 const app =express()
-
+const {login,register}=require("./controller/user.controller")
 app.use(express.json())
+
+app.post("/user",register)
+app.post("/login",login)
 
 app.listen(2345,async()=>{
     await connect()
